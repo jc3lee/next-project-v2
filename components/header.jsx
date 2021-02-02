0deca9240d37fbcd96e960c80f41a0c7d5f454b9
jclee <dev.if.ljc@gmail.com>
2021-02-02 19:41:33 +0100

    ➕twitter card

--- components/header.jsx
+++ components/header.jsx
@@ -1,9 +1,10 @@
 import Head from "next/head"
-
-const Header = ({ title = "Welcome!" }) => {
+const WEBSITE_DESCRIPTION = "Cybersecurity Conference 2021 (Project by JC Lee - Twitter @ljc_dev)"
+const Header = ({ title = "Cybersecurity Conference 2021 - Register now!" }) => {
   return (
     <Head>
       <title>{title}</title>
+      <meta name="description" content={WEBSITE_DESCRIPTION} />
       <link rel="shortcut icon" href="favicon.ico" />
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
